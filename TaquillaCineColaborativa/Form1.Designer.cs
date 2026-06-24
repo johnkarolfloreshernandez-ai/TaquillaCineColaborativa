dabb839ea66e3d02d35820822a7bad00c6a0a5d8
JohnKarolFloresHernandez <johnkarolfloreshernandez@gmail.com>
2026-06-24 16:15:12 -0600

    Se creo el formulario y la clase abstracta de Boleto

--- TaquillaCineColaborativa/Form1.Designer.cs
+++ TaquillaCineColaborativa/Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class frmCine
     {
-        /// <summary>
-        /// Variable del diseñador necesaria.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Limpiar los recursos que se estén usando.
-        /// </summary>
-        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,28 +13,112 @@
             base.Dispose(disposing);
         }
 
-        #region Código generado por el Diseñador de Windows Forms
-
-        /// <summary>
-        /// Método necesario para admitir el Diseñador. No se puede modificar
-        /// el contenido de este método con el editor de código.
-        /// </summary>
         private void InitializeComponent()
         {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblTipoBoleto = new System.Windows.Forms.Label();
+            this.cmbTipoBoleto = new System.Windows.Forms.ComboBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
+            this.lblTitulo.Location = new System.Drawing.Point(120, 20);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(191, 24);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "🎬 Taquilla de Cine";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTipoBoleto
+            // 
+            this.lblTipoBoleto.AutoSize = true;
+            this.lblTipoBoleto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblTipoBoleto.Location = new System.Drawing.Point(30, 70);
+            this.lblTipoBoleto.Name = "lblTipoBoleto";
+            this.lblTipoBoleto.Size = new System.Drawing.Size(171, 17);
+            this.lblTipoBoleto.TabIndex = 1;
+            this.lblTipoBoleto.Text = "Selecciona tipo de boleto:";
+            // 
+            // cmbTipoBoleto
+            // 
+            this.cmbTipoBoleto.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTipoBoleto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.cmbTipoBoleto.Items.AddRange(new object[] {
+            "Estudiante",
+            "Adulto Mayor",
+            "General"});
+            this.cmbTipoBoleto.Location = new System.Drawing.Point(196, 67);
+            this.cmbTipoBoleto.Name = "cmbTipoBoleto";
+            this.cmbTipoBoleto.Size = new System.Drawing.Size(180, 24);
+            this.cmbTipoBoleto.TabIndex = 2;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.btnCalcular.ForeColor = System.Drawing.Color.White;
+            this.btnCalcular.Location = new System.Drawing.Point(130, 110);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(150, 40);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular Precio";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.lblResultado.Location = new System.Drawing.Point(30, 180);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(111, 17);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "Total a pagar:";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
+            this.txtResultado.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.txtResultado.Location = new System.Drawing.Point(130, 172);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(150, 29);
+            this.txtResultado.TabIndex = 5;
+            this.txtResultado.Text = "$0.00";
+            this.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // frmCine
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(400, 240);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.cmbTipoBoleto);
+            this.Controls.Add(this.lblTipoBoleto);
+            this.Controls.Add(this.lblTitulo);
             this.Name = "frmCine";
-            this.Text = "Taquilla de cine";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Taquilla de Cine";
+            this.Load += new System.EventHandler(this.frmCine_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
-        #endregion
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblTipoBoleto;
+        private System.Windows.Forms.ComboBox cmbTipoBoleto;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtResultado;
     }
 }
 
